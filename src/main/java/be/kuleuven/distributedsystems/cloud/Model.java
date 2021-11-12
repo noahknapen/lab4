5af4a95c1aed8544cbdf5c06fbc5a9b3a4acf058
noahknapen <noah.knapen@student.kuleuven.be>
2021-11-12 21:03:43 +0100

    initial commit with distinction between manager and user

---
+++ src/main/java/be/kuleuven/distributedsystems/cloud/Model.java
@@ -0,0 +1,60 @@
+package be.kuleuven.distributedsystems.cloud;
+
+import be.kuleuven.distributedsystems.cloud.entities.*;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Component
+public class Model {
+
+    public List<Show> getShows() {
+        // TODO: return all shows
+        return new ArrayList<>();
+    }
+
+    public Show getShow(String company, UUID showId) {
+        // TODO: return the given show
+        return null;
+    }
+
+    public List<LocalDateTime> getShowTimes(String company, UUID showId) {
+        // TODO: return a list with all possible times for the given show
+        return new ArrayList<>();
+    }
+
+    public List<Seat> getAvailableSeats(String company, UUID showId, LocalDateTime time) {
+        // TODO: return all available seats for a given show and time
+        return new ArrayList<>();
+    }
+
+    public Seat getSeat(String company, UUID showId, UUID seatId) {
+        // TODO: return the given seat
+        return null;
+    }
+
+    public Ticket getTicket(String company, UUID showId, UUID seatId) {
+        // TODO: return the ticket for the given seat
+        return null;
+    }
+
+    public List<Booking> getBookings(String customer) {
+        // TODO: return all bookings from the customer
+        return new ArrayList<>();
+    }
+
+    public List<Booking> getAllBookings() {
+        // TODO: return all bookings
+        return new ArrayList<>();
+    }
+
+    public Set<String> getBestCustomers() {
+        // TODO: return the best customer (highest number of tickets, return all of them if multiple customers have an equal amount)
+        return null;
+    }
+
+    public void confirmQuotes(List<Quote> quotes, String customer) {
+        // TODO: reserve all seats for the given quotes
+    }
+}
---
+++ src/main/java/be/kuleuven/distributedsystems/cloud/auth/SecurityFilter.java
@@ -0,0 +1,102 @@
+package be.kuleuven.distributedsystems.cloud.auth;
+
+import be.kuleuven.distributedsystems.cloud.entities.User;
+import com.auth0.jwt.JWT;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var session = WebUtils.getCookie(request, "session");
+        if (session != null) {
+            // (level 1) decode Identity Token and assign correct email and role
+            var jwt = JWT.decode(session.getValue());
+            var email = jwt.getClaim("email").asString();
+            var role = jwt.getClaim("role").asString();
+            System.out.println(email + role);
+            // TODO: (level 2) verify Identity Token
+
+
+
+            var user = new User(email, role);
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(new FirebaseAuthentication(user));
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        return path.equals("/authenticate") || path.endsWith(".html") || path.endsWith(".js") || path.endsWith(".css");
+    }
+
+    private static class FirebaseAuthentication implements Authentication {
+        private final User user;
+
+        FirebaseAuthentication(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            if (user.isManager()) {
+                return List.of(new SimpleGrantedAuthority("manager"));
+            } else{
+                return new ArrayList<>();
+            }
+        }
+
+        @Override
+        public Object getCredentials() {
+            return null;
+        }
+
+        @Override
+        public Object getDetails() {
+            return null;
+        }
+
+        @Override
+        public User getPrincipal() {
+            return this.user;
+        }
+
+        @Override
+        public boolean isAuthenticated() {
+            return true;
+        }
+
+        @Override
+        public void setAuthenticated(boolean b) throws IllegalArgumentException {
+
+        }
+
+        @Override
+        public String getName() {
+            return null;
+        }
+    }
+}
+
